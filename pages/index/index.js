8cb5b59c01242ceebe1dbe9cad63aaf9a9c0031b
chenyangao <cygeagle@foxmail.com>
2017-06-21 22:06:42 +0800

    扫码平台
    测试

---
+++ pages/index/index.js
@@ -0,0 +1,29 @@
+//index.js
+//获取应用实例
+Page({
+  data: {
+    inputShowed: false,
+    inputVal: ""
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  }
+});
