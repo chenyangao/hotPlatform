c02d5cc2587ba3c8ea0d219486d6ddce620da088
chenyangao <cygeagle@foxmail.com>
2017-06-23 18:09:24 +0800

    2017年6月23日1808

--- pages/user_manager/user_manager.js
+++ pages/user_manager/user_manager.js
@@ -120,7 +120,12 @@ Page({
   //事件处理函数
   bindViewTap: function () {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../user_manager/register/register'
     })
   },
+  register: function () {
+    wx.navigateTo({
+      url: '../user_manager/register/register'
+    })
+  } 
 })
